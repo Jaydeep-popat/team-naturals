aa1621622ee6fb5cfe8cd5b2f53847abac6e73d2
Jaydeep Popat <popatjaydeep21@gmail.com>
2026-08-21 23:28:06 +0530

    something is there

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -11,11 +11,7 @@ const LOCAL_API_BASE_URL = 'http://127.0.0.1:8000';
 function getApiBaseUrl() {
   const configuredUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-  if (!isServer && window.location.hostname.endsWith('teamnaturals.in')) {
-    if (!configuredUrl || /^https?:\/\/(?:localhost|127\.0\.0\.1)(?::\d+)?$/i.test(configuredUrl)) {
-      return LIVE_API_BASE_URL;
-    }
-  }
+  if (!isServer) return '';
 
   if (configuredUrl) return configuredUrl;
   if (process.env.NODE_ENV === 'production') return LIVE_API_BASE_URL;
